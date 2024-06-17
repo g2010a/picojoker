cc4278a43e61a9c0b411e0201581a5e6e4a75062
Armando Gutierrez <agutierr@adobe.com>
2024-06-17 08:36:47 +0200

    refactor: use async calls to avoid blocking input

--- main.py
+++ main.py
@@ -4,18 +4,15 @@
    OpenWeatherMap API key and WiFi connection details.
 2. Copy config_secrets.py and main.py to your Raspberry Pi Pico
 """
-
-# for display
+# Openweather never actually worked, so using open-meteo instead
 import ujson
 import random
 import pimoroni
-from picographics import PicoGraphics, DISPLAY_PICO_DISPLAY
-from picoscroll import PicoScroll
-
-# for weather
 import gc
 import network
 import urequests
+import uasyncio as asyncio
+from picographics import PicoGraphics, DISPLAY_PICO_DISPLAY
 
 SCREEN_CONFIG = {
     "display": DISPLAY_PICO_DISPLAY,
@@ -30,9 +27,14 @@ LED_CONFIG = {
 }
 
 WEATHER_CONFIG = {
-    "city": "Hamburg",
-    "country_code": "DE",
-    "url_template": "http://api.openweathermap.org/data/2.5/weather?q={city},{country_code}&appid={api_key}",
+    "lat": 53.55,  # Latitude of Hamburg
+    "lon": 9.99,   # Longitude of Hamburg
+    "current": "temperature_2m,weather_code",
+    "hourly": "temperature_2m,precipitation_probability,precipitation,weather_code",
+    "daily": "weather_code,temperature_2m_max,temperature_2m_min,precipitation_hours,precipitation_probability_max",
+    "timezone": "Europe/Berlin",
+    "forecast_days": 1,
+    "url_template": "https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current={current}&hourly={hourly}&daily={daily}&timezone={timezone}&forecast_days={forecast_days}",
 }
 
 LOCAL_JOKE_FILE = "jokes.min.json"
@@ -45,272 +47,253 @@ BUTTON_B = pimoroni.Button(13)
 BUTTON_X = pimoroni.Button(14)
 BUTTON_Y = pimoroni.Button(15)
 
+# Initialize display
+display = PicoGraphics(display=SCREEN_CONFIG["display"], rotate=SCREEN_CONFIG["rotate"])
+display.set_font(SCREEN_CONFIG["font"])
+display.set_backlight(SCREEN_CONFIG["backlight"])
+width, height = display.get_bounds()
+pen_white = display.create_pen(255, 255, 255)
+pen_black = display.create_pen(0, 0, 0)
+pen_red = display.create_pen(255, 0, 0)
+font_scale = SCREEN_CONFIG["font_scale"]
 
-class Screen:
-    def __init__(self, display_config):
-        print("Initializing screen...")
-        self.display = PicoGraphics(
-            display=display_config["display"], rotate=display_config["rotate"]
-        )
-        self.display.set_font(display_config["font"])
-        self.width, self.height = self.display.get_bounds()
-        self.pen_white = self.display.create_pen(255, 255, 255)
-        self.pen_black = self.display.create_pen(0, 0, 0)
-        self.pen_red = self.display.create_pen(255, 0, 0)
-        self.font_scale = display_config["font_scale"]
-        self.log_font_scale = 0
-        self.display.set_backlight(display_config["backlight"])
-        self.scroll = PicoScroll()
-        self.wrap_pixels = self.width
-        self.log_area_height = 16
-        self.log_area_y = self.height - self.log_area_height
-        self._draw_log_area()
-
-    def draw_text(self, x, y, text):
-        print(f"Drawing text at {x}, {y}: {text[0:35]}...")
-        self.display.set_pen(self.pen_white)
-        self.display.text(text, x, y, self.wrap_pixels, self.font_scale)
-        self.display.update()
-
-    def clear(self):
-        self.display.clear()
-
-    def update(self):
-        self.display.update()
-
-    def set_backlight(self, value):
-        self.display.set_backlight(value)
-
-    def get_bounds(self):
-        return self.display.get_bounds()
-
-    def create_pen(self, r, g, b):
-        return self.display.create_pen(r, g, b)
-
-    def log(self, text):
-        print(f"Drawing log text: {text}")
-        self.display.set_pen(self.pen_black)
-        self.display.rectangle(0, self.log_area_y, self.width, self.log_area_height)
-        self.display.set_pen(self.pen_white)
-        self.display.text(text, 0, self.log_area_y, self.wrap_pixels, self.log_font_scale)
-        self.display.update()
-
-    def _draw_log_area(self):
-        self.display.set_pen(self.pen_white)
-        self.display.line(0, self.log_area_y - 1, self.width, self.log_area_y)
-        self.display.update()
-
-
-#
-# Screen set up, initialize the display
-#
-print("Starting...")
-gc.collect()
-screen = Screen(SCREEN_CONFIG)
+# Initialize LED
 led = pimoroni.RGBLED(*LED_CONFIG["pins_rgb"])
 led.set_rgb(*LED_COLOR_IDLE)
-screen.draw_text(0, 0, "Initializing...")
 
 
-#
-# Joke classes
-#
+def draw_text(x, y, text):
+    display.set_pen(pen_white)
+    display.text(text, x, y, width, font_scale)
+    display.update()
+
+
+def clear_screen():
+    display.set_pen(pen_black)
+    display.clear()
+    display.update()
+
+
+def log(text):
+    print(f"LOG: {text}")
+    display.set_pen(pen_black)
+    display.rectangle(0, height - 16, width, 16)
+    display.set_pen(pen_white)
+    display.text(text, 0, height - 16, width, 1)
+    display.update()
+
+
 class BaseJoke:
-    def fetch(self):
+    def __init__(self):
+        pass
+
+    async def fetch(self):
         raise NotImplementedError
 
-    def display(self, screen):
+    def display(self):
         raise NotImplementedError
-    
+
     def _sanitize(self, text):
         # Attempt to convert common unicode characters to ASCII, like quotes, backticks, accents, etc.
-        text = text.replace("„", "\"").replace("“", "\"").replace("‚", "'")\
-            .replace("‘", "'").replace("’", "'").replace("´", "'")\
-            .replace("`", "'").replace("–", "-").replace("—", "-")\
-            .replace("…", "...").replace("´", "'")
-        # Replace German umlauts with ASCII equivalents
-        text = text.replace("ä", "ae").replace("ö", "oe").replace("ü", "ue")\
-            .replace("Ä", "Ae").replace("Ö", "Oe").replace("Ü", "Ue")\
-            .replace("ß", "ss")
-        # Replace french accents with ASCII equivalents
-        text = text.replace("à", "a").replace("â", "a").replace("é", "e")\
-            .replace("è", "e").replace("ê", "e").replace("ë", "e")\
-            .replace("î", "i").replace("ï", "i").replace("ô", "o")\
-            .replace("ù", "u").replace("û", "u").replace("ç", "c")
-        # Replace Spanish accents with ASCII equivalents
-        text = text.replace("á", "a").replace("í", "i").replace("ó", "o")\
-            .replace("ú", "u").replace("ñ", "n")
-        # Remove any non-ASCII characters
-        text = "".join(i for i in text if ord(i) < 128)
-        return text
+        replacements = {
+            "–": "-", "—": "-", "…": "...", "‘": "'", "’": "'", "‚": "'",
+            "“": '"', "„": '"', "`": "'", "´": "'",
+            "á": "a", "à": "a", "â": "a", "ä": "ae", "Ä": "Ae", "ç": "c",
+            "é": "e", "è": "e", "ê": "e", "ë": "e", "í": "i", "î": "i",
+            "ï": "i", "ñ": "n", "ó": "o", "ô": "o", "ö": "oe", "Ö": "Oe",
+            "ß": "ss", "ú": "u", "ù": "u", "û": "u", "ü": "ue", "Ü": "Ue",
+        }
+        for k, v in replacements.items():
+            text = text.replace(k, v)
+        return "".join(i for i in text if ord(i) < 128)
 
 
 class OnlineGermanPunchlineJoke(BaseJoke):
-    """Fetches a joke from the WitzAPI."""
     def __init__(self):
-        self.joke = ""
-        self.url = 'https://witzapi.de/api/joke'
-    
-    def fetch(self):
-        screen.log("Fetching witz...")
+        super().__init__()
+        self.joke = None
+        self.url = "https://witzapi.de/api/joke"
+        self.error = None
+
+    async def fetch_url(self):
+        with urequests.get(self.url) as response:
+            return response
+
+    async def fetch(self):
+        log("Fetching witz...")
         gc.collect()
         led.set_rgb(*LED_COLOR_BUSY)
-        response = urequests.get(self.url)
-        json_response = response.json()
-        self.joke = self._sanitize(json_response[0]["text"])
-        led.set_rgb(*LED_COLOR_IDLE)
-    
-    def display(self, screen):
-        screen.display.set_pen(screen.pen_black)
-        screen.display.clear()
-        screen.display.set_pen(screen.pen_white)
-        screen.draw_text(0, 0, self.joke)
-        screen.display.update()
+        try:
+            response = await asyncio.get_event_loop().create_task(self.fetch_url())
+            json_response = ujson.loads(response.text)
+            self.joke = self._sanitize(json_response[0]["text"])
+        except Exception as e:
+            self.error = e
+        finally:
+            led.set_rgb(*LED_COLOR_IDLE)
 
+    def display(self):
+        clear_screen()
+        if self.error:
+            draw_text(0, 0, f"Error: {self.error}")
+            log(f"Error: {self.error}")
+            return
+        draw_text(0, 0, self.joke)
 
 class LocalPunchlineJoke(BaseJoke):
-    """Reads a local JSON file for a joke.
-    
-    Expected format: [["setup", "punchline"], ...]
-    """
     def __init__(self, filename):
+        super().__init__()
         self.setup = ""
         self.punchline = ""
         self.filename = filename
+        self.error = None
 
-    def fetch(self):
+    async def fetch(self):
         gc.collect()
-        screen.log("Reading local joke...")
+        log("Reading local joke...")
         led.set_rgb(*LED_COLOR_BUSY)
-        with open(self.filename, 'r', encoding="utf-8") as f:
-            count_lines = sum(1 for _ in f)
-        random_line_number = random.randint(1, count_lines)
-        print(f"Reading line {random_line_number} / {count_lines}")
-        with open(self.filename, 'r') as f:
-            for current_line_number, line in enumerate(f, start=1):
-                if current_line_number == random_line_number:
-                    joke = ujson.loads(line)
-                    self.setup = self._sanitize(joke[0])
-                    self.punchline = self._sanitize(joke[1])
-                    led.set_rgb(*LED_COLOR_IDLE)
-                    break
-
-    def display(self, screen):
-        screen.display.set_pen(screen.pen_black)
-        screen.display.clear()
-        screen.display.set_pen(screen.pen_white)
-        screen.draw_text(0, 0, self.setup + "\n---\n" + self.punchline)
-        screen.display.update()
-
-#
-# Weather class
-#
-class Weather:
-    def __init__(self, config, api_key):
-        self.city = config["city"]
-        self.country_code = config["country_code"]
-        self.url_template = config["url_template"]
-        self.api_key = api_key
-        self.weather_data = None
-
-    def get_weather(self, screen):
-        """ Get weather data from OpenWeatherMap"""
-        # Sample response:
-        #   {
-        #     "coord": {"lon": 10, "lat": 53.55},
-        #     "weather": [
-        #       { "id": 800, "main": "Clear", "description": "clear sky", "icon": "01d" }
-        #     ],
-        #     "base": "stations",
-        #     "main": {
-        #       "temp": 283.56,
-        #       "feels_like": 282.8,
-        #       "temp_min": 282.56,
-        #       "temp_max": 284.98,
-        #       "pressure": 1017,
-        #       "humidity": 82
-        #     },
-        #     "visibility": 10000,
-        #     "wind": {"speed": 3.6, "deg": 250},
-        #     "clouds": {"all": 0},
-        #     "dt": 1718259937,
-        #     "sys": { "type": 1, "id": 1263, "country": "DE", "sunrise": 1718247029, "sunset": 1718308194 },
-        #     "timezone": 7200,
-        #     "id": 2911298,
-        #     "name": "Hamburg",
-        #     "cod": 200
-        #   }
-        url = self.url_template.format(
-            city=self.city,
-            country_code=self.country_code,
-            api_key=self.api_key,
-        )
-        redacted_url = url.replace(self.api_key, "<REDACTED>")
-        screen.log(f"Requesting weather data from {redacted_url}")
         try:
-            gc.collect()
-            response = urequests.get(url)
+            with open(self.filename, "r", encoding="utf-8") as f:
+                jokes = [line.strip() for line in f]
+            random_joke = random.choice(jokes)
+            joke = ujson.loads(random_joke)
+            self.setup = self._sanitize(joke[0])
+            self.punchline = self._sanitize(joke[1])
         except Exception as e:
-            screen.log(f"Error: {e}")
+            self.error = e
+        finally:
+            led.set_rgb(*LED_COLOR_IDLE)
+
+    def display(self):
+        clear_screen()
+        if self.error:
+            draw_text(0, 0, f"Error: {self.error}")
+            log(f"Error: {self.error}")
             return
-        print(response)
-        return response.json()
-
-    def display(self, screen):
-        screen.display.clear()
-        screen.draw_text(0, 0, "Location: " + self.weather_data['name'])
-        screen.draw_text(0, 16, "Temperature: " + str(self.weather_data['main']['temp']) + "°C")
-        screen.draw_text(0, 32, "Humidity: " + str(self.weather_data['main']['humidity']) + "%")
-        screen.draw_text(0, 48, "Pressure: " + str(self.weather_data['main']['pressure']) + " hPa")
-        screen.display.update()
+        draw_text(0, 0, self.setup + "\n---\n" + self.punchline)
 
 
-# weather = Weather(WEATHER_CONFIG, OPENWEATHERMAP_API_KEY)
-# weather.get_weather(screen)
-# weather.display(screen)
+class Weather:
+    def __init__(self, config):
+        self.config = config
+        self.url_template = config["url_template"]
+        self.url = self.url_template.format(**config)
+        self.weather_data = None
+        self.request_headers = {"User-Agent": "curl/8.6.0"}
+        self.error = None
 
-# Initial joke fetch
-online_german_joke = OnlineGermanPunchlineJoke()
-local_joke = LocalPunchlineJoke(LOCAL_JOKE_FILE)
-local_joke.fetch()
-local_joke.display(screen)
+    async def fetch_url(self):
+        with urequests.get(self.url, headers=self.request_headers) as response:
+            return response
+    
+    async def fetch_weather(self):
+        log(f"Requesting weather data from {self.url}")
+        gc.collect()
+        led.set_rgb(*LED_COLOR_BUSY)
+        try:
+            response = await asyncio.get_event_loop().create_task(self.fetch_url())
+            self.weather_data = ujson.loads(response.text)
+        except Exception as e:
+            self.error = e
+        finally:
+            led.set_rgb(*LED_COLOR_IDLE)
 
-#
-# WiFi setup
-#
-screen.log("Reading secrets")
-led.set_rgb(*LED_COLOR_BUSY)
+    def display(self):
+        if self.error:
+            draw_text(0, 0, f"Error: {self.error}")
+            log(f"Error: {self.error}")
+            return
+        clear_screen()
+        draw_text(0, 0, f"{self.weather_data["current"]["time"]}")
+        draw_text(0, 16, self._wmo_weather_code_string(self.weather_data["current"]["weather_code"]))
+        draw_text(0, 32, f"Temp: {self.weather_data["current"]["temperature_2m"]}°C")
+        draw_text(0, 48, f"Rain: {self.weather_data["daily"]["precipitation_hours"][0]}mm")
+        draw_text(0, 64, f"Rain: {self.weather_data["daily"]["precipitation_probability_max"][0]}%")
+        display.update()
+
+    @staticmethod
+    def _wmo_weather_code_string(code):
+        codes = {
+            0: "Clear",
+            1: "Partly Cloudy",
+            2: "Cloudy",
+            3: "Overcast",
+            45: "Fog",
+            48: "Fog+",
+            51: "Light Showers",
+            53: "Showers",
+            55: "Heavy Showers",
+            56: "Freezing drizzle",
+            57: "Freezing drizzle+",
+            61: "Light rain",
+            63: "Rain",
+            65: "Heavy rain",
+            66: "Freezing rain",
+            67: "Freezing rain+",
+            71: "Light snow showers",
+            73: "Snow showers",
+            75: "Heavy snow showers",
+            77: "Sleet showers",
+            80: "Light snow",
+            81: "Snow",
+            83: "Heavy snow",
+            85: "Sleet",
+            86: "Sleet+",
+            95: "Thunderstorm",
+            96: "Thunderstorm hail",
+            99: "Thunderstorm hail+",
+        }
+        return codes.get(code, f"Unknown weather code ({code})")
+
+
+async def connect_wifi():
+    log("Reading secrets")
+    led.set_rgb(*LED_COLOR_BUSY)
+    try:
+        from config_secrets import WIFI_SSID, WIFI_PASS
+    except ImportError as exc:
+        log(
+            "Error: Please provide your WiFi connection details in config_secrets.py"
+        )
+        raise ImportError from exc
+
+    station = network.WLAN(network.STA_IF)
+    station.active(True)
+    log(f"Connecting to WiFi {WIFI_SSID}")
+    station.connect(WIFI_SSID, WIFI_PASS)
+
+    while not station.isconnected():
+        await asyncio.sleep(1)
+
+    log(f"Connected to WiFi {WIFI_SSID}")
+    log(str(station.ifconfig()))
+    led.set_rgb(*LED_COLOR_IDLE)
+
+
+async def main():
+    # Initial joke fetch
+    local_joke = LocalPunchlineJoke(LOCAL_JOKE_FILE)
+    await local_joke.fetch()
+    local_joke.display()
+
+    online_german_joke = OnlineGermanPunchlineJoke()
+    weather = Weather(WEATHER_CONFIG)
+
+    await connect_wifi()
+    while True:
+        if BUTTON_Y.read():
+            await local_joke.fetch()
+            local_joke.display()
+        elif BUTTON_X.read():
+            await online_german_joke.fetch()
+            online_german_joke.display()
+        elif BUTTON_A.read():
+            await weather.fetch_weather()
+            weather.display()
+        await asyncio.sleep(0.1)
+
+
 try:
-    from config_secrets import OPENWEATHERMAP_API_KEY, WIFI_SSID, WIFI_PASS
-except ImportError as exc:
-    print(
-        "Error: Please provide your OpenWeatherMap API key and WiFi connection details in config_secrets.py"
-    )
-    raise ImportError from exc
-
-station = network.WLAN(network.STA_IF)
-station.active(True)
-screen.log(f"Connecting to WiFi {WIFI_SSID}")
-station.connect(WIFI_SSID, WIFI_PASS)
-
-while station.isconnected() is False:
+    asyncio.run(main())
+except KeyboardInterrupt:
     pass
-
-screen.log(f"Connected to WiFi {WIFI_SSID}")
-screen.log(str(station.ifconfig()))
-led.set_rgb(*LED_COLOR_IDLE)
-
-# Main loop
-while True:
-    if BUTTON_Y.read():
-        local_joke.fetch()
-        local_joke.display(screen)
-    if BUTTON_X.read():
-        online_german_joke.fetch()
-        online_german_joke.display(screen)
-
-
-# TODO: Sleep most of the time, wake up every hour to fetch data
-# TODO: Refresh weather on Y
-# TODO: Turn screen on for a bit on any button press
-# TODO: Rotate screen on A or B
